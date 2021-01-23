08ed4dbff6a152559800e0a18ae60a9fc1130da8
umiranda <71298635+umiranda@users.noreply.github.com>
2021-01-23 15:01:24 -0500

    Final commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,21 @@
-// Use D3 to read the JSON file
+// Reading the file
 d3.json("Data/samples.json").then((bbData) => {
     window.bbData = bbData;
     console.log(bbData);
     var data = bbData;
   
-    // Add ID#s to dropdown menu
+    // Dropdown menu - ID#
     var idList = data.names;
     for (var i = 0; i < idList.length; i++) {
       selectBox = d3.select("#selDataset");
       selectBox.append("option").text(idList[i]);
     }
-  
-    // Set up default plot
+    // updating plots  
     updatePlots(0)
-  
-    // Function for updating plots   
     function updatePlots(index) {
   
   
-      // Set up arrays for horizontal bar chart & gauge chart
+      // arrays for horizontal bar chart & gauge chart
       var sampleSubjectOTUs = data.samples[index].otu_ids;
       console.log(sampleSubjectOTUs);
       var sampleSubjectFreq = data.samples[index].sample_values;
@@ -28,12 +25,12 @@ d3.json("Data/samples.json").then((bbData) => {
       console.log(washFrequency);
   
   
-      // Populate Demographic Data card
+    
       var demoKeys = Object.keys(data.metadata[index]);
       var demoValues = Object.values(data.metadata[index])
       var demographicData = d3.select('#sample-metadata');
   
-      // clear demographic data
+      // clear  data
       demographicData.html("");
   
       for (var i = 0; i < demoKeys.length; i++) {
@@ -49,7 +46,7 @@ d3.json("Data/samples.json").then((bbData) => {
       var topTenLabels = topTenOTUS.map((otu => "OTU " + otu));
       var reversedLabels = topTenLabels.reverse();
   
-      // Set up trace
+      // First trace
       var trace1 = {
         x: topTenFreq,
         y: reversedLabels,
@@ -59,10 +56,10 @@ d3.json("Data/samples.json").then((bbData) => {
         orientation: "h"
       };
   
-      // data
+      
       var barData = [trace1];
   
-      // Apply  layout
+      // layout
       var layout = {
         title: "Top 10 OTUs",
         margin: {
@@ -76,7 +73,7 @@ d3.json("Data/samples.json").then((bbData) => {
       // Render the plot to the div tag with id "plot"
       Plotly.newPlot("bar", barData, layout);
   
-      // Set up trace
+      // Second trace
       trace2 = {
         x: sampleSubjectOTUs,
         y: sampleSubjectFreq,
@@ -89,10 +86,10 @@ d3.json("Data/samples.json").then((bbData) => {
         }
       }
   
-      //data
+      
       var bubbleData = [trace2];
   
-      // Apply layout
+      // layout
       var layout = {
         title: 'OTU Frequency',
         showlegend: false,
@@ -106,31 +103,37 @@ d3.json("Data/samples.json").then((bbData) => {
       // Gauge chart
   
       var trace3 = [{
-        domain: {x: [0, 1], y: [0,1]},
-        type: "indicator",
-        mode: "gauge+number",
-        value: washFrequency,
-        title: { text: "Belly Button Washes Per Week" },
-        gauge: {
-          axis: { range: [0, 9], tickwidth: 0.5, tickcolor: "black" },
-          bar: { color: "#669999" },
-          bgcolor: "white",
-          borderwidth: 2,
-          bordercolor: "transparent",
-          steps: [
-            { range: [0, 1], color: "#fff" },
-            { range: [1, 2], color: "#e6fff5" },
-            { range: [2, 3], color: "ccffeb" },
-            { range: [3, 4], color: "b3ffe0" },
-            { range: [4, 5], color: "#99ffd6" },
-            { range: [5, 6], color: "#80ffcc" },
-            { range: [6, 7], color: "#66ffc2" },
-            { range: [7, 8], color: "#4dffb8" },
-            { range: [8, 9], color: "#33ffad" }
+       domain: {x: [0, 1], y: [0,1]},
+       type: "indicator",
+       mode: "gauge",
+       value: washFrequency,
+       title: { text: "Belly Button Washes Per Week" },
+       gauge: {
+         axis: { range: [0, 9], tickwidth: 0.5, tickcolor: "black" },
+         bar: { color: "#669999" },
+         bgcolor: "white",
+         borderwidth: 2,
+         bordercolor: "transparent",
+         steps: [
+           { range: [0, 1], color: "#f7fbff" },
+           { range: [1, 2], color: "#ebf3fb" },
+           { range: [2, 3], color: "#deebf7" },
+           { range: [3, 4], color: "#d2e3f3" },
+           { range: [4, 5], color: "#c6dbef" },
+           { range: [5, 6], color: "#b3d2e9" },
+           { range: [6, 7], color: "#85bcdb" },
+           { range: [7, 8], color: "#6baed6" },
+           { range: [8, 9], color: "#57a0ce" }
   
-          ],
-        }
-      }];
+         ],
+         threshold: {
+            line: { color: "red", width: 10 },
+            thickness: 0.75,
+            value: washFrequency,
+          }
+       }
+     }];
+       
   
       gaugeData = trace3;
   
@@ -147,14 +150,12 @@ d3.json("Data/samples.json").then((bbData) => {
     // On button click, call refreshData()
     d3.selectAll("#selDataset").on("change", refreshData);
   
-  
-  
     function refreshData() {
       var dropdownMenu = d3.select("#selDataset");
-      // Assign the value of the dropdown menu option to a variable
+      // Assign the value of the dropdown menu 
       var personsID = dropdownMenu.property("value");
       console.log(personsID);
-      // Initialize an empty array for the person's data
+      // Initialize an empty array for number chosen 
       console.log(data)
   
       for (var i = 0; i < data.names.length; i++) {
